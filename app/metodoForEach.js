98449706d8acc8a2d2884d0c9bd3e726ae1296a8
Pedro Souto Duarte Junior <pedro.duartejunior@gmail.com>
2023-07-21 14:54:06 -0300

    Finalização

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,15 +1,20 @@
 const elementoParaInserirLivros = document.getElementById ('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirOslivrosNaTela (listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+  elementoParaInserirLivros.innerHTML = ''
+
   listaDeLivros.forEach(livro => {
+    let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
     elementoParaInserirLivros.innerHTML += `
     <div class="livro">
-    <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+    <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
     <h2 class="livro__titulo">
       ${livro.titulo}
     </h2>
     <p class="livro__descricao">${livro.autor}</p>
-    <p class="livro__preco" id="preco">R$${livro.preco}</p>
+    <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
     <div class="tags">
       <span class="tag">${livro.categoria}</span>
     </div>
